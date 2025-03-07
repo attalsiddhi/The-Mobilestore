6ac55d03aeda1208f2a8d8885ac20ced0e00a244
attalsiddhi <siddhiattal317@gmail.com>
2025-03-07 09:40:14 +0530

    Project Updated

--- Mobilestore_microservice/jwtsecurity/src/main/java/com/abc/jwtsecurity/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abc.jwtsecurity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.abc.jwtsecurity.entity.RoleEntity;
-
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-
-}
